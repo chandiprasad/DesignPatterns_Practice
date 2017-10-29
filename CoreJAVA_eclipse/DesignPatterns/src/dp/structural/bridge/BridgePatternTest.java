e634519df54420d4a0b3d72cd6c775ca583aad20
chandiprasad <chandi.prasad@gmail.com>
2017-10-29 13:25:33 +0530

    committing my practice code

---
+++ CoreJAVA_eclipse/DesignPatterns/src/dp/structural/bridge/BridgePatternTest.java
@@ -0,0 +1,10 @@
+package dp.structural.bridge;
+
+public class BridgePatternTest {
+    public static void main(String[] args) {
+        Shape tri = new Triangle(new RedColor());
+        tri.applyColor();
+        Shape pent = new Pentagon(new GreenColor());
+        pent.applyColor();
+    }
+}
